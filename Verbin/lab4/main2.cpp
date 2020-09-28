a1bedd1c8a39ea043bc0cd51eaa295489c670529
nevermore20 <71935808+nevermore20@users.noreply.github.com>
2020-09-28 17:41:25 +0300

    Update main2.cpp
    Добавлена многопоточность

--- Verbin/lab4/main2.cpp
+++ Verbin/lab4/main2.cpp
@@ -6,13 +6,13 @@
 #include <climits>
 
 bool INTER = true;
-
 //перегруженны оператор вывода вектора в поток
 std::ostream& operator<<(std::ostream& stream, std::vector<int> vect) {
     stream << vect[0];
     for (int i = 1; i < vect.size(); i++) {
         stream << "," << vect[i];
-    } return stream;
+    }
+    return stream;
 }
 
 
@@ -49,38 +49,70 @@ void prefixFunction(std::string& sample, std::vector<int>& prefix) {
 }
 
 
+std::vector< std::pair<std::string, int >> toFlows(std::string text, const int uFlow, int maxFlow) {
+    
+    std::vector< std::pair<std::string, int >> tmp;
+    int step = maxFlow / uFlow;
+    int ind = 0;
+    for (int i = 0; i < uFlow; i ++) {
+        int len;
+        if (uFlow - i == 1) {
+            len = text.length() - ind;
+        }
+        else {
+            len = step + text.length()- maxFlow;
+        }
+        tmp.push_back(std::make_pair(text.substr(ind, len) ,ind));
+        ind += step;
+        std::cout <<"Добавлен поток: " <<tmp[tmp.size() - 1].first << " Начальный индекс: " << tmp[tmp.size() - 1].second << std::endl;
+    }
 
+    return tmp;
+}
 //функция определения сдвига
 bool findEntry(std::string& text, std::string& sample, std::vector<int>& prefix) {
     int k = 0;   //индекс сравниваемого символа в sample
+    bool flag = false;
     if (text.length() != sample.length()) {
         std::cout << "\nНе является циклическим сдвигом: ";
         std::cout << "-1";
         return false;
     }
-
-    text = text + text;              //удваиваем строку
-
-    for (int i = 0; i < text.length(); i++) {
-        while (k > 0 && sample[k] != text[i])
-            k = prefix[k - 1];
-
-        if (sample[k] == text[i])
-            k = k + 1;
-
-        if (k == sample.length()) {
-            std::cout << "Является сдвигом, позиция: ";
-            std::cout << i - sample.length() + 1;
-            return true;
-        }
-
+    text = text + text;
+    int maxFlow = text.length() - (sample.length() - 1);
+    std::cout << "Максимальное колличество потоков: " << maxFlow << " | Введите колличество потоков: ";
+    int uFlow;
+    std::cin >> uFlow;
+    if (uFlow > maxFlow || uFlow < 0) {
+        std::cout << "Ошибка в вводе! | Введите колличество потоков: ";
+        std::cin >> uFlow;
     }
+    else {
+        std::vector< std::pair<std::string, int >> flows = toFlows(text, uFlow, maxFlow);
+        std::cout << std::endl;
+        for (int j = 0; j < flows.size(); j++) {
+            std::cout << "Поиск циклического сдвига в потокe: " << flows[j].first << ";" << std::endl;
+            for (int i = 0; i < flows[j].first.length(); i++) {
+                while (k > 0 && sample[k] != flows[j].first[i])
+                    k = prefix[k - 1];
 
-    std::cout << "\nНе является циклическим сдвигом: ";
-    std::cout << "-1";
-    return false;
+                if (sample[k] == flows[j].first[i])
+                    k = k + 1;
+                else if (k != 0) k = prefix[k - 1];
+                if (k == sample.length()) {
+                    std::cout << "\n\tНайден циклический сдвиг! позиция: " << flows[j].second + i - sample.length() + 1 << std::endl;
+                    flag = true;
+                }
 
-
+            }
+        } 
+        if (!flag) {
+            std::cout << "\nНе является циклическим сдвигом: ";
+            std::cout << "-1";
+        }
+       
+    } 
+    return flag;
 }
 
 
@@ -89,10 +121,10 @@ int main()
     setlocale(LC_ALL, "Russian");
     std::string text;
     std::string sample;
-    std::cin >> text;
-    std::cin >> sample;
+    std::cin >> sample; std::cin >> text;
     std::vector<int> prefix(sample.length());
     prefixFunction(sample, prefix);
+     
     findEntry(text, sample, prefix);
     return 0;
 }
